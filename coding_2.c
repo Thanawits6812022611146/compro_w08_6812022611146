1e466f67457c2b3d43073da92fc8f9d3c7ed887a
Thanawits6812022611146 <s6812022611146@email.kmutnb.ac.th>
2025-09-09 12:52:06 +0700

    Update coding_2.c

--- coding_2.c
+++ coding_2.c
@@ -6,12 +6,12 @@ int main() {
     int start_number, stop_number;
     printf("Enter start number: "); // ใส่ตัวเลขเริ่มต้น
     scanf("%d", &start_number);
-    printf("Enter stop number: "); // ใส่ตัวเลขที่หยุด
+    printf("Enter stop number: "); // ใส่ตัวเลขที่สิ้นสุด
     scanf("%d", &stop_number);
     printf("number start %d is number stop %d:\n", start_number, stop_number);
     printf("-------------\n");
 
-    for (int i = start_number; i <= stop_number; i++) { // ตัวเลขเริ่มต้นน้อยกว่าหรือเท่ากับตัวเลขที่จะหยุด
+    for (int i = start_number; i <= stop_number; i++) { // ตัวเลขเริ่มต้นน้อยกว่าหรือเท่ากับตัวเลขที่สิ้นสุด
        
     }printf("sequential numbers are: ");
 
@@ -23,4 +23,5 @@ int main() {
     }
    printf("thanks you.\n");
     return 0;
+
 }
